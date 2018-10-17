f7d51f9dc3ee9d628119d61ae0b00944df10d766
1natsu172 <1natsu172@users.noreply.github.com>
2018-10-17 22:52:10 +0900

    add bash style bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -42,4 +42,9 @@ function fish_user_key_bindings
         bind -M insert \cg '__ghq_crtl_g'
     end
     ### ghq ###
+    ### Bash Style Command ###
+    # https://github.com/fish-shell/fish-shell/wiki/Bash-Style-Command-Substitution-and-Chaining-(!!-!%24-&&-%7C%7C)
+    bind ! bind_bang
+    bind '$' bind_dollar
+    ### Bash Style Command ###
 end
