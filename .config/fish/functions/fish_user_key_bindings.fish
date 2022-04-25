6846db1825d70cbe4e5961e143d4aae13df40faa
1natsu <1natsu172@users.noreply.github.com>
2022-04-25 18:42:30 +0900

    update: some changes caused by `fisher update`

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,4 @@
 function fish_user_key_bindings
-    fzf_key_bindings
     ### Bash Style Command ###
     # https://github.com/fish-shell/fish-shell/wiki/Bash-Style-Command-Substitution-and-Chaining-(!!-!%24-&&-%7C%7C)
     bind ! bind_bang
