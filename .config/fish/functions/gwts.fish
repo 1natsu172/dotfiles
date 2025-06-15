fef44491d930913b533164b032c6209b63685e25
1natsu <1natsu172@users.noreply.github.com>
2025-06-15 21:57:31 +0900

    feat: exclude current worktree from selection in gwts function

--- .config/fish/functions/gwts.fish
+++ .config/fish/functions/gwts.fish
@@ -10,7 +10,9 @@ function gwts -d "Switch to a git worktree using fzf"
         return 1
     end
 
-    set selected_worktree (git worktree list | fzf --height=40% --reverse --border --prompt="Select worktree: " | awk '{print $1}')
+    # 現在のworktreeは除外して表示
+    set current_worktree (pwd)
+    set selected_worktree (git worktree list | grep -v "^$current_worktree " | fzf --height=40% --reverse --border --prompt="Select worktree: " | awk '{print $1}')
 
     if test -n "$selected_worktree"
         cd "$selected_worktree"
